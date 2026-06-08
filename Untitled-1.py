96ebbfd061eee9962686d7dc51e03b823d255bdf
mo2tafa2 <sukonamostafa@gmail.com>
2026-06-08 14:35:57 +0300

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,14 +4,29 @@ item3 = 300
 budget = 500
 total_price = item1 + item2 + item3
 print (total_price)
-
-
 if total_price <= budget:
     print ("you can buy all items")
 if total_price > budget:
     print ("you cannot buy all items")
-
-
-    
     difference = total_price - budget
     print (difference)
+    michel_olise = 150
+vitinha = 140
+joao_neves = 140
+budget = 200
+total = michel_olise + vitinha + joao_neves
+print (total)
+if joao_neves < budget and joao_neves > michel_olise:
+    print ("joao_neves to real madrid here we go")
+    if vitinha < michel_olise and michel_olise < budget:
+        print ("michel_olise to real madrid here we go")
+        if vitinha < budget and vitinha > michel_olise:
+            print ("vitinha to real madrid here we go")
+            if total < budget:
+                print ("all super players have a chance to go to real madrid here we go")
+                print ("hala madrid")
+                print ("we are the best team in the world")
+                greating = "welcome to real madrid"
+                print (greating)
+                print ("writen by mostafa abdelmonem abdo ashour")
+                print ("the end")
